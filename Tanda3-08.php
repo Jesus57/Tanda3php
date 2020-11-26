e0af077b57946e7a660c834cd563667efe25d3ac
DESKTOP-DOOLO0V\Jesu\github <jesusrosadopereira@gmail.com>
2020-11-26 16:31:30 +0100

    1.0

---
+++ Tanda3-08.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>07</title>
+  </head>
+  <body>
+          </form>
+        <?php
+          if (!isset($_POST['n']))
+          {
+            echo "Por favor, introduzca un número del 0 al 10:";
+            echo '<form action="Tanda3-08.php" method="post">';
+            echo '<input type="number" name="n" min="0" max="10"<br>';
+            echo '<input type="submit" value="Aceptar">';
+          }
+          else
+          {
+            
+            $n = $_POST['n'];
+            echo '<table border=1px>';
+            for($i=0;$i<=10;$i++)
+            {
+              echo '<tr><td>'. $n ." * ".$i. " = ". $n*$i . '</tr></td>';
+            }
+            echo '</table>';
+          }
+          
+        ?>
+  </body>
+</html>
