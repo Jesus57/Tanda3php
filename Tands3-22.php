34e97ab55598c05439de1bdedf66bce02712e49a
DESKTOP-DOOLO0V\Jesu\github <jesusrosadopereira@gmail.com>
2020-12-01 17:57:21 +0100

    1.0

---
+++ Tands3-22.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>22.php</title>
+  </head>
+  <body>
+        <?php
+          echo "Números primos entre 2 y 100: <br>";
+
+          for ($n = 2; $n <= 100; $n++) {
+            $esPrimo = true;
+            for ($i = 2; $i < ($n/2)+1; $i++) {
+              if ($n % $i == 0) {
+                $esPrimo = false;
+              }
+            }
+            if ($esPrimo) {
+              echo "$n<>";
+            }
+          }
+        ?>
+        <br><br>
+  </body>
+</html>
